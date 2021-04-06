3f05f15a4bbb2cb5a7406743d8b1eed2e9623303
tirin2411 <nutrinh2411@gmail.com>
2021-04-06 13:54:28 +0700

    Lab 02

---
+++ Lab02/Lab2THJava/src/lab2thjava/Bai13.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2thjava;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+
+/**
+ *
+ * @author Admin
+ */
+public class Bai13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static void modifyFile(String filePath, String oldString, String newString) {
+        File fileToBeModified = new File(filePath);
+        String oldContent = " ";
+        BufferedReader reader = null;
+        FileWriter writer = null;
+        try {
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+            //Reading all the lines of input text file into oldContent
+            String line = reader.readLine();
+            while (line != null) {
+                oldContent = oldContent + line + System.lineSeparator();
+                line = reader.readLine();
+            }
+            //Replacing oldString with newString in the oldContent
+            String newContent = oldContent.replaceAll(oldString, newString);
+            //Rewriting the input text file with newContent
+            writer = new FileWriter(fileToBeModified);
+            writer.write(newContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //Closing the resources
+                reader.close();
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        modifyFile("D:\\HocKy2N4\\ThucHanhJava\\Lab2\\Lab2THJava\\text.txt", "OldString", "NewString");
+        System.out.println("done");
+    }
+    
+}
